4865db050df118ec051f8286a431b1d8c7ec61aa
Vinicius Salles de Oliveira <56527570+ViniiSalles@users.noreply.github.com>
2024-10-27 18:58:05 -0300

    Update VideoRequestDTO.java

--- PROJETOS/YouTubeVideoDownloader/src/main/java/com/example/YouTubeVideoDownloader/dto/VideoRequestDTO.java
+++ PROJETOS/YouTubeVideoDownloader/src/main/java/com/example/YouTubeVideoDownloader/dto/VideoRequestDTO.java
@@ -21,3 +21,19 @@ public class VideoRequestDTO {
         this.path = path;
     }
 }
+
+// Melhoria para esse codigo: Usar o metodo record. Ele cria automaticamente todos os getters, setter, construtores, etc, para uma classe DTO !!
+
+// public record VideoRequestDTO(String url, String path) { // Esse é o necessario. Abaixo disso é complemento, nesse caso estou fazendo uma verificação dos campos.
+//     public VideoRequestDTO {
+//         if (url == null || url.isBlank()) {
+//             throw new IllegalArgumentException("URL não pode ser nula ou vazia");
+//         }
+//         if (path == null || path.isBlank()) {
+//             throw new IllegalArgumentException("Path não pode ser nulo ou vazio");
+//         }
+//     }
+// }
+
+
+
